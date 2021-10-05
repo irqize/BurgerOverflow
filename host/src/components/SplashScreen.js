e5d360b957ae063e4414cf341527d2d61dc17f39
Amalia <aberglof@kth.se>
2021-10-05 16:28:33 +0200

    updated tree

--- host/src/components/SplashScreen.js
+++ host/src/components/SplashScreen.js
@@ -11,13 +11,16 @@ const SplashScreen = () => {
 
   return (
     <>
-    <div className="advertisement-0">
-      <p>Hungry for a new <text className="advertisement-text-red">experience</text>?</p>
-    </div>
     <div className='splashContainer'>
+    <div className="advertisement-0">
+      <p className="advertisement-text-head">Win your lunch <br/>on the fly.</p>
+      <p className="advertisement-text-bod">Just scan it.</p>
+    </div>
+      <div className="burgerFlex">
       <img id="BurgerBunUp" src={BurgerBunUp}/>
       <QRCode value={process.env.NODE_ENV === 'production' ? process.env.DOMAIN : 'https://' + window.location.hostname + ':3000'} renderAs='svg' />
       <img id="BurgerBunDown" src={BurgerBunDown}/>
+      </div>
     </div>
     </>
   )
